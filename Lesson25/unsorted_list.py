80bae4950185a6af64493662566a0f1a7f843a82
Daryna Skryp <daryna.skryp@siemens.com>
2024-02-11 20:53:58 +0100

    Add Lesson25: implement slicing for unsorted list

---
+++ Lesson25/unsorted_list.py
@@ -0,0 +1,124 @@
+class Node:
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    def get_data(self):
+        return self._data
+
+    def get_next(self):
+        return self._next
+
+    def set_data(self, data):
+        self._data = data
+
+    def set_next(self, new_next):
+        self._next = new_next
+
+
+class UnsortedList:
+
+    def __init__(self):
+        self._head = None
+
+    def is_empty(self):
+        return self._head is None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self._head)
+        self._head = temp
+
+    def size(self):
+        current = self._head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.get_next()
+
+        return count
+
+    def search(self, item):
+        current = self._head
+        found = False
+        while current is not None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+
+        return found
+
+    def remove(self, item):
+        current = self._head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if previous is None:
+            self._head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+    def __repr__(self):
+        representation = "<UnsortedList: "
+        current = self._head
+        while current is not None:
+            representation += f"{current.get_data()} "
+            current = current.get_next()
+        return representation + ">"
+
+
+def slice_unsorted_list(unsorted_list, start=0, stop=0):
+    if (start or stop) < 0 or stop > unsorted_list.size():
+        raise ValueError("Index out of range")
+    head = unsorted_list._head
+    if not head:
+        return
+    curr_pos = 0
+    current_node = head
+    sliced_list = UnsortedList()
+    while curr_pos < start:
+        current_node = current_node.get_next()
+        curr_pos += 1
+    while curr_pos < stop:
+        sliced_list.add(current_node.get_data())
+        current_node = current_node.get_next()
+        curr_pos += 1
+    return sliced_list
+
+
+if __name__ == "__main__":
+    my_list = UnsortedList()
+
+    my_list.add(31)
+    my_list.add(77)
+    my_list.add(17)
+    my_list.add(93)
+    my_list.add(26)
+    my_list.add(54)
+
+    print(my_list.size())
+    print(my_list)
+    print(my_list.search(93))
+    print(my_list.search(100))
+
+    my_list.add(100)
+    print(my_list.search(100))
+    print(my_list.size())
+
+    my_list.remove(54)
+    print(my_list.size())
+    my_list.remove(93)
+    print(my_list.size())
+    my_list.remove(31)
+    print(my_list.size())
+    print(my_list.search(93))
+    print(my_list)
+
+    print(slice_unsorted_list(my_list))
